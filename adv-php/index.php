ba52761ef9b1d664021fa510e359a2ce842ead45
ramashish1812 <ahram1807@gmail.com>
2025-09-06 15:11:54 +0530

    Add files via upload

---
+++ adv-php/index.php
@@ -0,0 +1,4 @@
+<!--  Write a program to demonstrate Restaurant Table Reservation System-.
+ Prepare demonstration of crud operations with Task List Manager under
+software development principles and follow coding protocols.
+ In this Task Manager application 4 Functionalities are there. -->
