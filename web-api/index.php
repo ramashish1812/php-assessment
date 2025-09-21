d2086b2ca39cd97322d9b10349fd6744b37f05b2
ramashish1812 <ahram1807@gmail.com>
2025-09-21 18:10:13 +0530

    Add files via upload

---
+++ web-api/index.php
@@ -0,0 +1,3 @@
+<!-- API Endpoints : REST endpoints: /api/login, /api/grades, /api/subjects 
+Security : Sanctum auth with student token. Error if student tries to access another’s data. 
+Pagination : Key Skills Assessed: Use custom pagination and ordering.  -->
