f5ca41de0bb084132dc4ca83c18c321cea351774
ramashish1812 <ahram1807@gmail.com>
2026-01-02 09:36:00 +0530

    Add files via upload

---
+++ Final Capstone Project/index.php
@@ -0,0 +1,3 @@
+<!-- User Auth:- Set up Laravel with Breeze auth. 
+CRUD Operations:- Create CRUD for freelancers and job posts. Add user status (active/blocked) with soft deletes. 
+Stats Dashboard:- Add chart stats: total jobs, users, blocked users.  -->
